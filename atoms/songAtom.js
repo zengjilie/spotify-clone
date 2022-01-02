f88ea8718ba84906a4b01769363a3fcc63f2e297
zengjilie <zengjilie@gmail.com>
2022-01-02 00:05:54 -0600

    ..

---
+++ atoms/songAtom.js
@@ -0,0 +1,11 @@
+import { atom } from "recoil";
+
+export const currentTrackIdState = atom({
+    key:'currentTrackIdState',
+    default:null
+})
+
+export const isPlayState= atom({
+    key:'currentTrackIdState',
+    default:null
+})
